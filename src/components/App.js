841234fb2d41e0414ed1157c1541ccb368a95dca
BlueCodeThree <carliemhamilton@gmail.com>
2020-01-15 10:46:03 +1000

    Adds routing to App

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,14 @@ import {
     BrowserRouter as Router,
     Switch
 } from "react-router-dom";
-import Nav from "../Nav/Nav";
-import HomePage from "../HomePage/HomePage"
-import Blog from "../Blog/Blog";
-import BlogPost from "../BlogPost/BlogPost";
-import UserDashboard from "../UserDashboard/UserDashboard";
-import UserLogin from "../UserLogin/UserLogin";
-import UserRegister from "../UserRegister/UserRegister";
-import UserWishlist from "../UserWishlist/UserWishlist";
+import Nav from "./Nav/Nav";
+import HomePage from "./HomePage/HomePage"
+import Blog from "./Blog/Blog";
+import BlogPost from "./BlogPost/BlogPost";
+import UserDashboard from "./UserDashboard/UserDashboard";
+import UserLogin from "./UserLogin/UserLogin";
+import UserRegister from "./UserRegister/UserRegister";
+import UserWishlist from "./UserWishlist/UserWishlist";
 
 class App extends Component {
     constructor(props) {
@@ -27,13 +27,13 @@ class App extends Component {
                 <Nav loggedInUser={loggedInUser} />
 
                 <Switch>
-                    <Route path="/wishlist/:id" component={UserWishlist} />
+                    {/* <Route path="/wishlist/:id" component={UserWishlist} />
                     <Route path="/register" component={UserRegister} />
                     <Route path="/login" component={UserLogin} />
                     <Route path="/dashboard/:id" component={UserDashboard} />
                     <Route path="/blog/:id" component={BlogPost} />
-                    <Route path="/blog" component={Blog} />
-                    <Route exact path="/" component={HomePage} />
+                    <Route path="/blog" component={Blog} /> */}
+                    {/* <Route exact path="/" component={HomePage} /> */}
                 </Switch>
 
             </Router>
