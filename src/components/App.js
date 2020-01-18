740552f6bf62acd679e3a869718f5e4dc3996322
Tony <huynh.tony55@gmail.com>
2020-01-18 14:22:36 +1000

    resolved conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
-import React, { useState, useReducer } from "react";
+import React, { Component } from "react";
 import {
     Link,
     Route,
     BrowserRouter as Router,
-    Switch,
-    Redirect
+    Switch
 } from "react-router-dom";
-import stateReducer from "../config/stateReducer"
 import Nav from "./Nav/Nav";
 import HomePage from "./HomePage/HomePage"
 import Blog from "./Blog/Blog";
@@ -15,88 +13,48 @@ import UserDashboard from "./UserDashboard/UserDashboard";
 import UserLogin from "./UserLogin/UserLogin";
 import UserRegister from "./UserRegister/UserRegister";
 import UserWishlist from "./UserWishlist/UserWishlist";
-import { Container } from "react-bulma-components";
+import {Container} from "react-bulma-components";
 
 import Vitamin from "./Vitamin/Vitamin";
-import { loginUser, logoutUser } from "../services/authServices";
 
-const App = () => {
-    const [loggedInUser, dispatchLoggedInUser] = useReducer(stateReducer, null);
-    const [loginError, dispatchLoginError] = useReducer(stateReducer, null);
-
-     function handleLogin(event, props) {
-        event.preventDefault();
-        const form = event.target;
-        const username = form.elements.username.value
-        const password = form.elements.password.value
-
-        loginUser({
-            username: username,
-            password: password
-        })
-            .then(() => {
-                dispatchLoggedInUser({
-                    type: "setLoggedInUser",
-                    data: username
-                })
-                setLoggedInUser(username)
-                props.history.push("/")
-            })
-            .catch((error) => {
-                const status = error.response ? error.response.status : 500
-                console.log(`An error occurred authenticating: ${error} with status: ${status}`)
-                dispatchLoginError({
-                    type: "setLoginError",
-                    data: "Authentication failed! Check your username and password"
-            })
-        })
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loggedInUser: null
+        }
     }
+    render() {
+        const { loggedInUser } = this.state
+        return (
+            <Router>
+                <Container>
+                    <Nav loggedInUser={loggedInUser} />
+                    <Link to="/blog">Blog</Link>
+                    <br />
+                    <Link to="/vitamin">Vitamin</Link>
+                    <br />
+                    <Link to="/wishlist">My Wishlist</Link>
+                    <br />
+                    <Link to="/dashboard">Dashboard</Link>
 
-    function setLoggedInUser(user) {
-        user ? localStorage.setItem("loggedInUser", user) : localStorage.removeItem("loggedInUser")
+                <Switch>
+                    <Route exact path="/vitamin/:id" component={Vitamin} />
+                    <Route path="/vitamin/" component={Vitamin} />
+
+                    {/* This should be wishlist/:id */}
+                     <Route path="/wishlist/" component={UserWishlist} />
+                    {/*<Route path="/register" component={UserRegister} />*/}
+                    <Route path="/login" component={UserLogin} />
+                    <Route path="/dashboard/:id" component={UserDashboard} />
+                    <Route path="/blog/:id" component={BlogPost} />
+                    <Route path="/blog" component={Blog} />
+                    <Route exact path="/" component={HomePage} />
+                </Switch>
+                </Container>
+            </Router>
+        )
     }
-
-    function handleLogout() {
-        logoutUser()
-        dispatchLoggedInUser({
-            type: "setLoggedInUser",
-            data:  null
-        })
-        setLoggedInUser(null)
-        return <Redirect to="/" />
-    }
-
-
-
-    return (
-        <Router>
-            <Container>
-                <Nav loggedInUser={loggedInUser} />
-                <Link to="/blog">Blog</Link>
-                <br />
-                <Link to="/vitamin">Vitamin</Link>
-                <br />
-        <Link to="/wishlist">My Wishlist</Link>
-
-            <Switch>
-                {/* this should be /vitamin/:id */}
-                <Route path="/vitamin/" component={Vitamin} />
-
-                {/* This should be wishlist/:id */}
-                    <Route path="/wishlist/" component={UserWishlist} />
-                <Route path="/register" component={UserRegister} />
-                <Route path="/login" render={ (props) => <UserLogin {...props} handleLogin={handleLogin} loginError={loginError} />} />
-                <Route path="/logout" render={() => handleLogout()} />
-                {/* <Route path="/dashboard/:id" component={UserDashboard} /> */}
-                <Route path="/blog/:id" component={BlogPost} />
-                <Route path="/blog" component={Blog} />
-                <Route exact path="/" component={HomePage} />
-            </Switch>
-            </Container>
-        </Router>
-    )
 }
-
-
 
 export default App
--- src/components/Vitamin/Vitamin.js
+++ src/components/Vitamin/Vitamin.js
@@ -1,127 +1,61 @@
 import React, { Fragment } from "react";
-import { Section } from "react-bulma-components";
-
+import { Section,Image } from "react-bulma-components";
+import Axios from "axios";
 
 // need to get id for an individual item
 
 export class Vitamin extends React.Component {
     constructor(props) {
         super(props)
+        this.mounted = false;
         this.state = {
-            loggedInUser: null,
-            vitamin: this.mockVitamin
+            vitamins: []
         }
     }
 
-    mockVitamin = {
-        "productName": "BIOSOURCE Women's Mega Multivitamin",
-        "link": "https://www.priceline.com.au/weight-and-vitamins/vitamins/multivitamins-and-minerals/biosource-women-s-mega-multivitamin-120-capsules",
-        "vitaminInformation": [
-            {
-                "name": "Thiamine hydrochloride",
-                "amount": "75mg"
-            },
-            {
-                "name": "Riboflavin",
-                "amount": "75mg"
-            },
-            {
-                "name": "Nicotinamide",
-                "amount": "75mg"
-            },
-            {
-                "name": "Calcium pantothenate",
-                "amount": "69mg"
-                        },
-            {
-                "name": "Pyridoxine hydrochloride",
-                "amount": "62mg"
-            },
-            {
-                "name": "Biotin",
-                "amount": "100μg"
-            },
-            {
-                "name": "Folic acid",
-                "amount": "500μg"
-            },
-            {
-                "name": "Cyanocobalamin",
-                "amount": "50μg"
-            },
-            {
-                "name": "Calcium ascorbate dihydrate",
-                "amount": "200mg"
-            },
-            {
-                "name": "Colecalciferol (Vitamin D3)",
-                "amount": "12.5μg (500IU)"
-            },
-            {
-                "name": "d-alpha-Tocopheryl acid succinate",
-                "amount": "41.3mg (50IU)"
-            },
-            {
-                "name": "Inositol",
-                "amount": "20mg"
-            },
-            {
-                "name": "Calcium",
-                "amount": "40mg"
-            },
-            {
-                "name": "Iron",
-                "amount": "5mg"
-            },
-            {
-                "name": "Potassium",
-                "amount": "100μg"
-            },
-            {
-                "name": "Magnesium",
-                "amount": "20mg"
-            },
-            {
-                "name": "Manganese",
-                "amount": "1mg"
-            },
-            {
-                "name": "Chromium",
-                "amount": "45μg"
-            },
-            {
-                "name": "Selenium",
-                "amount": "26μg"
-            },
-            {
-                "name": "Silicon",
-                "amount": "30mg"
-            },
-            {
-                "name": "Zinc",
-                "amount": "12mg"
-            },
-            {
-                "name": "Panax ginseng",
-                "amount": "5mg"
-            }
-        ]
+    componentDidMount() {
+        Axios.request({
+            url: 'https://analyzevit-back.herokuapp.com/data',
+        })
+            .then(response => {
+                this.mounted = true;
+                // console.log(response.data)
+                this.setState({
+                    vitamins: response.data
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    componentWillUnmount() {
+        this.mounted = false;
     }
 
     render(){
-        const { vitamin } = this.state
+        const { vitamins } = this.state
         return(
             <Fragment>
                 <Section>
-                <p><b>Product Name:</b> <a href={vitamin.link}>{vitamin.productName}</a></p>
-                <p>{vitamin.vitaminInformation.map((info) => {
-                    return(
-                        <div>
-                            <p><b>Name:</b> {info.name}</p>
-                            <p><b>Amount:</b> {info.amount}</p>
-                        </div>
-                    )
-                })}</p>
+                {
+                    vitamins.map(vitamin => {
+                        return (
+                            <Section>
+                                <p><b>Product Name:</b><a href={vitamin.link}>{vitamin.productName}</a></p>
+                                <Image src={vitamin.image}/>
+                                {vitamin.vitaminInformation.map((info) => {
+                                    return (
+                                        <div>
+                                            <p><b>Name:</b> {info.name}</p>
+                                            <p><b>Amount:</b> {info.amount}</p>
+                                        </div>
+                                    )
+                                })}
+                            </Section>
+                        )
+                    })
+                }
                 </Section>
             </Fragment>
         )
