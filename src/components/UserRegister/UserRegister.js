933190ba7bb35b3a238e7fe34cccfe7d8140081d
BlueCodeThree <carliemhamilton@gmail.com>
2020-01-22 12:02:58 +1000

    adds commenting and refactors some code

--- src/components/UserRegister/UserRegister.js
+++ src/components/UserRegister/UserRegister.js
@@ -3,9 +3,13 @@ import { registerUser } from "../../services/authServices";
 import { styles } from "../../styles/styles";
 import { Heading, Columns, Section } from 'react-bulma-components';
 
+// Component handles user registration
 const RegisterUser = (props) => {
 
+	// Because this is the only component to use this particular hook, it is here in this file for state only for this component, rather than in app and the reducer for global state.
 	const [registerError, setRegisterError] = useState(null)
+
+	// function that gets the details from the form and sends it to the server
 	function handleRegister(event) {
 		event.preventDefault()
 		const form = event.target
@@ -15,7 +19,7 @@ const RegisterUser = (props) => {
 
 		registerUser({username: username, email: email, password: password}).then((response) => {
 			console.log("got response from register user on server:", response)
-			props.history.push("/login")
+			props.history.push("/")
 		}).catch((error) => {
 			const status = error.response ? error.response.status : 500
 			if(status === 409) {
@@ -26,6 +30,8 @@ const RegisterUser = (props) => {
 		})
 	}
 
+	// returns the registration form. When there is an error from the server, an error message is rendered.
+	// On submit it calls the handleRegister function
 	return (
 		<Section>
 			<Heading size={1}>Register an Account</Heading>
