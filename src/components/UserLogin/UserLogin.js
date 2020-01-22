f93d72ea4ab66b1886863f7d2053838521a0e63b
lelani82 <lelaniparker@hotmail.com>
2020-01-22 13:16:34 +1000

    added login test using Cypress

--- src/components/UserLogin/UserLogin.js
+++ src/components/UserLogin/UserLogin.js
@@ -9,21 +9,21 @@ const UserLogin = (props) => {
     return (
         <Section>
             <Heading size={1}>Log in</Heading>
-			<Heading subtitle size={5}>Welcome back</Heading>
+            <Heading subtitle size={5}>Welcome back</Heading>
             <Columns>
                 <Columns.Column>
-                    <form onSubmit={(event) => handleLogin(event, props)}>
-                        { loginError && <p className="has-text-danger">{ loginError }</p> }
+                    <form data-cy="loginForm" onSubmit={(event) => handleLogin(event, props)}>
+                        {loginError && <p className="has-text-danger">{loginError}</p>}
                         <label className="label" style={styles.padding}>Username</label>
-                        <input type="text" className="input" name="username" placeholder="Username" required></input>
+                        <input type="text" data-cy="username" className="input" name="username" placeholder="Username" required></input>
                         <label className="label" style={styles.padding}>Password</label>
-                        <input type="password" className="input" name="password" placeholder="Password" required></input>
-                        <input type="submit" value="Login" className="button is-info" style={styles.padding}></input>
+                        <input type="password" data-cy="password" className="input" name="password" placeholder="Password" required></input>
+                        <input type="submit" data-cy="loginButton" value="Login" className="button is-info" style={styles.padding}></input>
                     </form>
                 </Columns.Column>
                 <Columns.Column>
-					<img alt="A group of happy people" src="stockimage_happypeople.jpg" style={styles.photo}></img>
-				</Columns.Column>
+                    <img alt="A group of happy people" src="stockimage_happypeople.jpg" style={styles.photo}></img>
+                </Columns.Column>
             </Columns>
         </Section>
     )
